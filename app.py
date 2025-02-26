91f0cad55bc27da30926da81c203eaca14c69246
Vi Figueiredo <vifigueiredo@outlook.com.br>
2025-02-26 10:57:47 -0300

    `Update app.py to run on port 5010 instead of 5000`

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def exportar_xlsx():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
